56f14c4b771aafe11cf9feba640cf4fff69edd0e
julianvidela <Julianvyd@gmail.com>
2024-06-03 11:15:47 -0300

    new updates

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,16 +1,17 @@
 
-import HomeCard from "../components/home/homeCard";
+import HomeComponent from "../components/home/home";
 import NavBar from "../components/navbar/navbar";
-import ArticleSection from "@/components/article/articleSetion";
+
 
 export default function Home() {
   return (
    <main className="gradient">
-    <section className="h-screen m-auto max-w-[90%] w-[1300px]">
+    <section className="h-screen m-auto max-w-[90%] w-[1200px]">
 
     <NavBar/>
-    <HomeCard/>
-    <ArticleSection/>
+    <HomeComponent/>
+    
+    
     
     </section>
 
